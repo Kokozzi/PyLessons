2fecd52d6cc376e52051af745fd98f27161ca682
Maxim Kokov <maxim@marrow.pro>
2018-02-11 20:50:33 +0300

    check combinations

--- lesson6/less6_ex2.py
+++ lesson6/less6_ex2.py
@@ -16,10 +16,12 @@ class DeckClass:
                 self.card_pack.append(CardClass(card, suit))
         # Randomly shuffle the list of cards
         self.shuffle()
+
     # Shuffles list of cards randomly
     def shuffle(self):
         shuffle(self.card_pack)
         return self.card_pack
+
     # Picks card from list 
     def drop_card(self):
         return self.card_pack.pop()
@@ -31,6 +33,7 @@ class CardClass:
         self.card_displaying = card_number[1]
         self.card_ranking = card_number[0]
         self.suit = suit
+
     # Form proper representaion, ex.: "8 of Clubs"
     def __repr__(self):
         return str(self.card_displaying) + " of " + str(self.suit)
@@ -45,12 +48,14 @@ class PlayerClass:
         self.current_hand = []
         # Player's displaying name
         self.name = name
+
     # Appends card to list in hand limit is not reached
     def get_card(self, card):
         if len(self.current_hand) < self.hand_size:
             self.current_hand.append(card)
         else:
             raise ValueError("Hand is full")
+
     # Display list of cards in player's hand
     def show_hand(self):
         print("{}: {}".format(self.name, self.current_hand))
@@ -62,30 +67,35 @@ class CroupierClass:
         self.table = table
         self.players = []
         self.deck = deck
-        self.combination = None
-        self.winner = None
+        self.flush_winners = []
+        self.straight_winners = []
+        self.fours_winners = []
+
     def add_player(self, player):
         if len(self.players) < self.players_count:
             self.players.append(player)
         else:
             raise ValueError("Too much players for this table!")
+
     def start_game(self):
         for player in self.players:
             self.distribute_cards(player)
         self.distribute_cards(self.table)
         self.display_game()
-
         self.combinations_check()
+
     def distribute_cards(self, player):
         while True:
             try:
                 player.get_card(self.deck.drop_card())
             except ValueError:
                 break
+
     def display_game(self):
         for player in self.players:
             player.show_hand()
         self.table.show_hand()
+
     def combinations_check(self):
         suits_dict = {}
         for card in self.table.current_hand:
@@ -96,11 +106,11 @@ class CroupierClass:
                 suits_dict[card.suit] = {"count": 1, "max_card": card.card_ranking}
         for suit, counter in suits_dict.items():
             if counter["count"] >= 3:
-                is_flush, winner = self.check_flush(suit, counter)
-                print("Flush ", is_flush, winner)
+                self.check_flush(suit, counter)
                 break
-        is_straight, winner = self.check_straight()
-        print("Straight ", is_straight, winner)
+        self.check_straight()
+        self.get_winner()
+
     def check_flush(self, suit, count):
         players_result = {}
         winners_list = []
@@ -112,43 +122,64 @@ class CroupierClass:
                     players_result[player.name]["max_card"] = max(players_result[player.name]["max_card"], card.card_ranking)
             if players_result[player.name]["count"] == 5:
                 winners_list.append((player.name, players_result[player.name]["max_card"]))
-        winners_list = sorted(winners_list, key=lambda tup: tup[1])
-        if len(winners_list) > 0:
-            return True, winners_list[0][0]
-        else:
-            return False, None
+        self.flush_winners = sorted(winners_list, key=lambda tup: tup[1])
+
+    def get_ordered_hand(self, player):
+        ordered_hand = []
+        for card in player.current_hand:
+            ordered_hand.append(card.card_ranking)
+        for table_card in self.table.current_hand:
+            ordered_hand.append(table_card.card_ranking)
+        return(sorted(ordered_hand, reverse=True))
+
     def check_straight(self):
-        ordered_hands = {}
         winners_list = []
         for player in self.players:
-            ordered_hands[player.name] = []
-            for card in player.current_hand:
-                ordered_hands[player.name].append(card.card_ranking)
-            for table_card in self.table.current_hand:
-                ordered_hands[player.name].append(table_card.card_ranking)
-            ordered_hands[player.name] = sorted(ordered_hands[player.name], reverse=True)
+            player_hand = self.get_ordered_hand(player)
             fails_counter = 0
-            for element in ordered_hands[player.name]:
+            for element in player_hand:
                 n = 1
                 if fails_counter < 3:
                     while True:
-                        if element - n not in ordered_hands[player.name]:
+                        if element - n not in player_hand:
                             fails_counter += 1
                             break
                         elif n == 4:
                             winners_list.append((player.name, element))
+                            break
                         else:
                             n += 1
                 else:
                     break
+        self.straight_winners = sorted(winners_list, key=lambda tup: tup[1])
+    
+    def check_fours(self):
+        winners_list = []
+        self.fours_winners = sorted(winners_list, key=lambda tup: tup[1])
+    
+    def winner_check(self, winners_list, combination):
         winners_list = sorted(winners_list, key=lambda tup: tup[1])
         if len(winners_list) > 0:
-            return True, winners_list[0][0]
+            print("{} is winner with {}".format(winners_list[0][0], combination))
         else:
-            return False, None
-
-
-
+            print("No winner")
+    
+    def get_winner(self):
+        if len(self.straight_winners) > 0 and len(self.flush_winners) > 0:
+            if self.straight_winners[0][0] == self.flush_winners[0][0]:
+                if self.straight_winners[0][1] == 12:
+                    print("{} wins with ROYAL FLUSH!".format(self.straight_winners[0][0]))
+                else:
+                    print("{} wins with Straight Flush!".format(self.straight_winners[0][0]))
+            else:
+                print("{} wins with Flush!".format(self.flush_winners[0][0]))
+        elif len(self.straight_winners) > 0:
+            print("{} wins with Straight!".format(self.straight_winners[0][0]))
+        elif len(self.flush_winners) > 0:
+            print("{} wins with Flush!".format(self.flush_winners[0][0]))
+        else:
+            print("Winner unknown")
+            
 
 
 # Form card's deck
