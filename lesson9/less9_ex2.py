0dd502153d3847bc4eeab46b71d8b93021ebbf4c
Maxim Kokov <maxim@marrow.pro>
2018-02-21 12:25:06 +0300

    less9 ex2

--- lesson9/less9_ex2.py
+++ lesson9/less9_ex2.py
@@ -1,14 +1,31 @@
 import requests
+import re
 
+# Get page content from url and return it
 def get_site(url):
     site = requests.get(url)
-    print(site.text)
+    return site.text
 
+# Read local copy of reddit tread, saved to file
 def read_file(filename):
-    with open(filename) as f:
+    with open(filename, "r", encoding='utf-8') as f:
         return f.read()
 
-# get_site("https://www.reddit.com/r/Python/comments/7ymqs7/intro_to_data_science_with_python_new_course_to/")
+# Try to open url and save it content to var
+site_text = get_site("https://www.reddit.com/r/Python/comments/7ymqs7/intro_to_data_science_with_python_new_course_to/")
 
-reddit_file = read_file("./lesson9/reddit.txt")
+# Reddit connection block appeared, use local copy of thread instead
+if "Too Many Requests" in site_text:
+    print("Got too many requests error, use local copy\n")
+    site_text = read_file("save_reddit.txt")
 
+# Remove return symbols from page text
+prepared_text = re.sub(r'\n', '', site_text)
+# Find all appearances of re in page text
+result = re.findall(r'data-type="comment".*?data-author="(\w+)".*?<div class="md"><p>([^<]*)</p>', prepared_text)
+
+print("Comments:\n")
+# Print results in pretty format 
+for comment in result:
+    print("{}: {}".format(comment[0], comment[1]))
+    print()
