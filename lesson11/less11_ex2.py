3f05450fa8cdfe38a8cee9f7f494d58cb1b12475
kokozzi <madkokozzi@gmail.com>
2018-02-28 10:51:14 +0300

    less11_ex2

--- lesson11/less11_ex2.py
+++ lesson11/less11_ex2.py
@@ -1,49 +1,46 @@
 import datetime
 
-class TimeContext:
-    def __init__(self, func, *args, **kwargs):
+# Measuring elapsing time
+class TimeMeasureContext:
+    def __init__(self):
+        # Class attributes for storing start and finish time
         self.start_time = 0
         self.finish_time = 0
-        self.func = func
-        self.func_args = args
-        self.func_kwargs = kwargs
-    
+    # Detect start time and store it
     def __enter__(self):
         self.start_time = datetime.datetime.now()
-        return self.func(*self.func_args, **self.func_kwargs)
-    
+    # Code execution is finished, detect finish time
     def __exit__(self, *args):
         self.finish_time = datetime.datetime.now()
+        # Calculate result calculations time
         print("Elapsed time: {}".format(self.finish_time - self.start_time))
 
-
+# Supress all exceptions
 class ExceptionsContext:
-    def __init__(self, func, *args, **kwargs):
-        self.func = func
-        self.func_args = args
-        self.func_kwargs = kwargs
-    
-    def __enter__(self):
-        try:
-            result = self.func(*self.func_args, **self.func_kwargs)
-            return result
-        except Exception:
-            print("Exception appeared")
-            return None
-    
-    def __exit__(self, *args):
+    def __init__(self):
         pass
+    def __enter__(self):
+        pass
+    # Detect raised exceptions and supress them
+    def __exit__(self, exception_type, exception_value, exception__traceback):
+        # If `exception_type` is present - exception was rised during code execution
+        if exception_type is not None:
+            # Print some info about supressed exception
+            print("Exception was supressed: {}".format(exception_value))
+            # Supress exception
+            return True
 
 
-def test_sum(a, b):
-    return a + b
-
-
-with TimeContext(test_sum, 8989889198989, 8879121114444) as result:
-    print(result)
+# Time Measuring with help of context manager
+with TimeMeasureContext():
+    # Some example calculations
+    i = 0
+    while i < 10000:
+        i += 1
+    print("Count complete")
 
 print()
 
-with ExceptionsContext(test_sum, "error", 2) as new_result:
-    if new_result is not None:
-        print(new_result)
+# Exceptions supressing with help of context manager
+with ExceptionsContext():
+    a = 5 / 0
